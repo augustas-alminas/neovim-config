6c228b9e628748cab80ef8a76cc299d628b5f333
Augustas Alminas <augustasalminas2@gmail.com>
2025-03-19 23:42:31 +0100

    Initial commit

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -2,4 +2,14 @@
 --  I promise not to create any merge conflicts in this directory :)
 --
 -- See the kickstart.nvim README for more information
-return {}
+return {
+  {
+    'rockyzhang24/arctic.nvim',
+    branch = 'v2',
+    dependencies = { 'rktjmp/lush.nvim' },
+    config = function()
+      -- vim.cmd 'colorscheme arctic'
+    end,
+  },
+  { 'akinsho/toggleterm.nvim', version = '*', config = true },
+}
